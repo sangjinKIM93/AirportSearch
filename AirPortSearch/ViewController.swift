a42f50b45d369f4ada16798f099b03b80a8ed208
sangjinKIM93 <larger611@gmail.com>
2020-08-18 21:27:59 +0900

    Initial Commit

---
+++ AirPortSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirPortSearch
+//
+//  Created by 김상진 on 2020/08/18.
+//  Copyright © 2020 kipCalm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
